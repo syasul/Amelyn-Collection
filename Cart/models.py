9625899b5b7a7af4ce8e29db172d570b79a6d92b
syasul <githubku4@gmail.com>
2024-02-16 21:31:07 +0700

    feat(cart): add models cart

--- Cart/models.py
+++ Cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from .models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Cart {self.id_cart} - User: {self.user.username}"
+    
+
+class CartItem(models.Model):
+    id_cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    id_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    subtotal = models.IntegerField()
+    
+    def __str__(self):
+        return f"Item {self.id_cartitem} - Product: {self.product.name} - Quantity: {self.quantity} - Subtotal: {self.subtotal}"
+
+    
