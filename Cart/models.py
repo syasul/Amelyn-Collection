bcb0d823420707062f858fe1ffda38b5a16d8652
syasul <githubku4@gmail.com>
2024-02-17 01:02:01 +0700

    feat(cart): fix models cart

--- Cart/models.py
+++ Cart/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-from .models import Product
+from Product.models import Product
+from User.models import CustomUser
 
 # Create your models here.
 class Cart(models.Model):
-    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"Cart {self.id_cart} - User: {self.user.username}"
